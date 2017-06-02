f893d392250ee1b88d37e038d01463302f0eaf5d
Keith Hubert <keith.hubert@gmail.com>
2017-06-02 00:31:28 -0400

    completed language component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,18 +1,39 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state= {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+
+  }
+
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang
+      }
+    })
+  }
+
   render() {
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
     return(
       <ul className='languages'>
-        {languages.map(function (lang, idx) {
+        {languages.map(function (lang) {
           return(
-            <li key={idx}>
+            <li
+              style={lang === this.state.selectedLanguage ? { color: '#d0021b' }: null}
+              onClick={this.updateLanguage.bind(null, lang)}
+              key={lang}>
               {lang}
             </li>
           )
-        })}
+        }, this)}
       </ul>
     )
   }
